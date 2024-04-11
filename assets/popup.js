f7cd23318c407c39683728070366f71ca46a5561
Bohdan <podp17@gmail.com>
2024-04-11 10:40:34 +0300

    popup: ajax add to cart

--- assets/popup.js
+++ assets/popup.js
@@ -6,8 +6,20 @@ class Popup {
         this.popupShown = localStorage.getItem('popupShown');
         this.closeButton = document.querySelector('.js-popup-close');
 
+        this.products = Array.from(document.querySelectorAll('[data-product-id]'))
+            .map(product => ({
+                'id': parseInt(product.getAttribute('data-product-id'), 10),
+                'quantity': 1
+            }));
+
+        this.popupForm = document.querySelector('#popupForm');
+
         this.closeButton.addEventListener('click', () => {
             this.closePopup();
+        })
+        this.popupForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            this.addToCart();
         })
     }
 
@@ -16,7 +28,6 @@ class Popup {
             setTimeout(() => {
                 this.popup.style.display = 'block';
                 document.documentElement.classList.add('lock');
-
                 if (this.showOnce === 'true') {
                     localStorage.setItem('popupShown', 'true');
                 }
@@ -28,10 +39,45 @@ class Popup {
         this.popup.style.display = 'none';
         document.documentElement.classList.remove('lock');
     }
+
+    async addToCart() {
+        let formData = {
+            'items': this.products
+        };
+
+        try {
+            const response = await fetch('/cart/add', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+
+            console.log(response);
+
+            await this.updateCartCounter();
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    async updateCartCounter() {
+        try {
+            const response = await fetch("/cart.json");
+            const cart = await response.json();
+
+            document.querySelectorAll(".cart-count-bubble").forEach((el) => {
+                el.textContent = cart.item_count;
+            });
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 }
 
 addEventListener('DOMContentLoaded', () => {
-    const popupElement = document.querySelector('#js-popup');
+    const popupElement = document.querySelector('#popup');
     if (popupElement) {
         const myPopup = new Popup(popupElement);
         myPopup.showPopup();
